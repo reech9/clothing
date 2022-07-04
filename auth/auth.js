1d5990106c135fc812202f9fc5d0c462f73f7760
reech9 <reech.p8@gmail.com>
2022-07-04 18:28:00 +0545

    complete functionality for user guard

--- auth/auth.js
+++ auth/auth.js
@@ -1,35 +1,34 @@
 const jwt = require("jsonwebtoken");
 const customer = require("../models/customerModel");
 
-
-// creating function 
+// creating function
 // which works as a mediator
 
-module.exports.customerGuard = (req, res, next)=>{
-    try{
+module.exports.customerGuard = (req, res, next) => {
+  try {
     // token receive
-    const token = req.headers.authorization.split(" ")[1]
+    const token = req.headers.authorization.split(" ")[1];
     // token verification
     // the logged in user id is avaikable in data variable below
-    const data= jwt.verify(token, 'everest');
+    const data = jwt.verify(token, "everest");
     console.log(data);
 
     // check on database
-    customer.findOne({_id: data.customer_id})
-    .then((result)=>{
+    customer
+      .findOne({ _id: data.customer_id })
+      .then((result) => {
+        console.log(result);
         req.customerData = result;
         next();
-    })
-    .catch((e)=>{
+      })
+      .catch((e) => {
         res.json({
-            msg: "Invalid Token"
-        })
-    
-    })
-    }
-    catch(e){
-        res.json({msg: 'invalid access'})
-    }
-    // console.log(data);
-    // console.log(token);
-}
+          msg: "Invalid Token",
+        });
+      });
+  } catch (e) {
+    res.json({ msg: "invalid access" });
+  }
+  // console.log(data);
+  // console.log(token);
+};
