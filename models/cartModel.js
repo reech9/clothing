f853985ff79ad879cd5cd8871452a0ecefc88dc6
reech9 <reech.p8@gmail.com>
2022-07-04 18:22:27 +0545

    cart model completed

---
+++ models/cartModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Cart = new mongoose.Schema({
+  productId: { type: mongoose.SchemaTypes.ObjectId, ref: "Product" },
+  userId: { type: mongoose.SchemaTypes.ObjectId, ref: "Customer" },
+  quantity: { type: Number, required: true },
+  unitPrice: { type: Number, required: true },
+  totalPrice: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("Cart", Cart);
