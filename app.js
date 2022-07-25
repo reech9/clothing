34e13e438829b58c4c1461070d52cc24245b6ca7
reech9 <reech.p8@gmail.com>
2022-07-25 17:09:45 +0545

    add routes for different operations

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 
 const app = express();
-app.use(express.json())
+const path = require("path");
+
+app.use(express.json());
+// app.use(express.urlencoded()); // Accepting form data
 
 require("./connection/connection");
 
-const customerRoute= require("./routers/customerRouter");
+app.use("/upload", express.static(path.join(__dirname, "/upload")));
+
+const customerRoute = require("./routers/customerRouter");
+const productRoute = require("./routers/productRouter");
+const reviewRoute = require("./routers/reviewRouter");
+const cartRoute = require("./routers/cartRouter");
+
 app.use(customerRoute);
+app.use(productRoute);
+app.use(reviewRoute);
+app.use(cartRoute);
 
 app.listen(90);
