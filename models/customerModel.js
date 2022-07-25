0474011392d147d5c4dcee9b5e6eec52b93d04a7
reech9 <reech.p8@gmail.com>
2022-07-25 17:11:17 +0545

    format file

--- models/customerModel.js
+++ models/customerModel.js
@@ -1,36 +1,29 @@
 const mongoose = require("mongoose");
 
-const Customer= new mongoose.Schema({
-    fn:{
-        type: String
-    },
-    ln:{
-        type:  String
-    },
-    age:{
-        type: Number
-    },
-    dob:{
-        type:Date
-    },
-    password:{
-        type: String,
-        required: true     
-    },
-    email:{
-        type: String,
-        required: true
-    },
-    picture: {
-        type: String
-    },
+const Customer = new mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  dob: {
+    type: Date,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  // picture: {
+  //   type: String,
+  // },
+});
 
-    userId:{
-        type: mongoose.SchemaTypes.ObjectId,
-        ref: "Customer"
-    }
-
-
-})
-
-module.exports = mongoose.model('Customer', Customer);
+module.exports = mongoose.model("Customer", Customer);
