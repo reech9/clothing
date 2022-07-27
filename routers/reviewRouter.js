fbed58ef10a8a8aa05e1872a0f46f8b72c6a95e6
reech9 <reech.p8@gmail.com>
2022-07-27 06:56:20 +0545

    add route to get the review by id

--- routers/reviewRouter.js
+++ routers/reviewRouter.js
@@ -2,8 +2,13 @@ const express = require("express");
 const router = new express.Router();
 const { customerGuard } = require("../auth/auth");
 
-const { addReview, updateReview } = require("../controller/reviewController");
+const {
+  addReview,
+  updateReview,
+  getReviewByProductId,
+} = require("../controller/reviewController");
 
+router.get("/get/review/:id", getReviewByProductId);
 router.post("/review/givereview", customerGuard, addReview);
 router.put("/review/update/:id", customerGuard, updateReview);
 
